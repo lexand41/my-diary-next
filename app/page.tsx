abb95648e7172681b263fe599f128bfe2570c934
lexand41 <lexand41s@gmail.coml>
2024-02-28 10:23:31 +0700

    first commit

---
+++ app/page.tsx
@@ -0,0 +1,80 @@
+import DayState from "@/components/DayState";
+import Image from "next/image";
+
+export default function Home() {
+  const duties = {
+    // "daily Leetcode": {
+    //   "10.11.23": true,
+    //   "11.11.23": false,
+    //   "12.11.23": true,
+    // },
+    // "daily React": {
+    //   "10.11.23": false,
+    //   "11.11.23": true,
+    //   "12.11.23": true,
+    // },
+    // "daily Next JS": {
+    //   "10.11.23": true,
+    //   "11.11.23": false,
+    //   "12.11.23": false,
+    // },
+  };
+
+  const today = new Date();
+  const todayWeekDay = today.getDay();
+  const weekDays = ["Mon", "Tue", "Wed", "Thu", "Fri", "Sat", "Sun"];
+  // console.log("todayWeekDay ", todayWeekDay);
+
+  const sortedWeekDays = weekDays
+    .slice(todayWeekDay)
+    .concat(weekDays.slice(0, todayWeekDay));
+  const last7Days = weekDays
+    .map((_, index) => {
+      const date = new Date();
+      date.setDate(date.getDate() - index);
+      return date.toISOString().slice(0, 10);
+    })
+    .reverse();
+  console.log("last7Days ", last7Days);
+
+  return (
+    <main className="container relative flex flex-col gap-8 px-4 pt-16">
+      {duties === null ||
+        (Object.keys(duties).length === 0 && (
+          <h1 className="mt-20 text-4xl font-light text-white font-display text-center">
+            You have no registered duty
+          </h1>
+        ))}
+
+      {duties !== null &&
+        Object.entries(duties).map(([duty]) => (
+          <div key={duty} className="flex flex-col gap-2">
+            <div className="flex justify-between item-center">
+              <span className="text-xl font-light text-white font-sans">
+                {duty}
+              </span>
+              <button>
+                <Image
+                  src="/delete.svg"
+                  width={20}
+                  height={20}
+                  alt="Icon delete duty"
+                />
+              </button>
+            </div>
+
+            <section className="grid grid-cols-7 bg-neutral-800 rounded-md p-2">
+              {weekDays.map((day) => (
+                <div key={day} className="flex flex-col">
+                  <span className="font-sans text-center text-xs text-white">
+                    {day}
+                  </span>
+                  <DayState day={undefined} />
+                </div>
+              ))}
+            </section>
+          </div>
+        ))}
+    </main>
+  );
+}
---
+++ components/DayState.tsx
@@ -0,0 +1,15 @@
+import Image from "next/image";
+
+export default function DayState({ day }: { day: boolean | undefined }) {
+  let image: [string, string, number?] = ["/mark.svg", "gray mark", 12];
+
+  if (day === true) image = ["/check.svg", "green mark", 24];
+  if (day === false) image = ["/x.svg", "red mark", 24];
+
+  const [src, alt, size] = image;
+  return (
+    <div className="flex items-center justify-center h-9">
+      <Image src={src} width={size} height={size} alt={alt} />
+    </div>
+  );
+}
